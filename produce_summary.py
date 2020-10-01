3e01353568f54ef7f9c6099cd8b95ba19ef96bb5
Matilda Basner <tillyqb@pop-os.localdomain>
2020-10-01 09:43:56 -0400

    added comments as requested in the exercize instruction

--- produce_summary.py
+++ produce_summary.py
@@ -1,16 +1,16 @@
-reports = ["um-deliveries-20140519.txt", "um-deliveries-20140520.txt", "um-deliveries-20140521.txt"]
+reports = ["um-deliveries-20140519.txt", "um-deliveries-20140520.txt", "um-deliveries-20140521.txt"]    #creates a list of the report file names
 
-for day in range(len(reports)):
-    print(f"Day {day + 1}")
-    the_file = open(reports[day])
-    for line in the_file:
-        line = line.rstrip()
-        words = line.split('|')
+for day in range(len(reports)):                                                                         #starts the loop to iterate through the different reports
+    print(f"Day {day + 1}")                                                                             #prints the day for the report shown
+    the_file = open(reports[day])                                                                       #opens the file specified
+    for line in the_file:                                                                               #starts the loop to perform the bulk of the work
+        line = line.rstrip()                                                                            #turns a line of the file into a string
+        words = line.split('|')                                                                         #turns the string into a list
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        melon = words[0]                                                                                #defines variable 'melon' from the first item in the list
+        count = words[1]                                                                                #defines variable 'count' from the second item in the list
+        amount = words[2]                                                                               #defines variable 'amount' from the third item in the list
 
-        print("Delivered {} {}s for total of ${}".format(
+        print("Delivered {} {}s for total of ${}".format(                                               #print the line in the new format
             count, melon, amount))
-    the_file.close()
+    the_file.close()                                                                                    #close the file
